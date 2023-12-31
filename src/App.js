a7b7b4adf3ed6979dd57613ca89d3d87f2d863e6
Kiran S G <gskiran526@gmail.com>
2023-12-31 14:27:20 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Logo from "./Components/Logo/Logo";
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import Intro from './Components/Intro/Intro';
+import Practices from './Components/Practices/Practices';
+import Clients from './Components/Clients/Clients';
+import Team from './Components/Team/Team';
+import FAQ from './Components/FAQ/FAQ';
+import Subscribe from './Components/Subscribe/Subscribe';
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -11,6 +17,12 @@ function App() {
       {/* <Navbar/> */}
       <Header/>
       <Intro/>
+      <Practices/>
+      <Clients/>
+      <Team/>
+      <FAQ/>
+      <Subscribe/>
+      <Footer/>
     </div>
   );
 }
